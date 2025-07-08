39cf9aab8e16bd32549b89bc2afa11884d8e4baa
osso2k <ossoble817@gmal.com>
2025-07-08 03:48:22 +0300

    Playing around

--- frontend/src/components/Functionality.jsx
+++ frontend/src/components/Functionality.jsx
@@ -21,30 +21,30 @@ const Functionality = () => {
         setMsg("")
     }
   },[counter])
-function blue(){
-    setColor("bg-blue-500")
-}
-function yellow(){
-    setColor("bg-yellow-500")
-}
-function red(){
-    setColor("bg-red-500")
-}
-function pink(){
-    setColor("bg-pink-500")
-}
+// function blue(){
+//     setColor("bg-blue-500")
+// }
+// function yellow(){
+//     setColor("bg-yellow-500")
+// }
+// function red(){
+//     setColor("bg-red-500")
+// }
+// function pink(){
+//     setColor("bg-pink-500")
+// }
 
   return (
     <div className={`flex mx-auto h-screen justify-center items-center text-white ${color}`}>
-        <div className='flex  mr-5'>
-            <div className='mr-4' onMouseOver={blue}>Blue</div>
+        <div className='flex flex-col mr-5'>
+            {/* <div className='mr-4' onMouseOver={blue}>Blue</div>
             <div className='mr-4' onMouseOver={yellow}>yellow</div>
             <div className='mr-4'  onMouseOver={red}>Red</div>
-            <div  onMouseOver={pink}>Pink</div>
-            {/* <p>{counter}</p>
+            <div  onMouseOver={pink}>Pink</div> */}
+            <p>{counter}</p>
             <button onClick={add}>Increment</button>
             <button onClick={dec}>Decrement</button>
-            <p>{msg}</p> */}
+            <p>{msg}</p>
 
         </div>
     </div>
