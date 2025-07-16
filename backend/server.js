c44a5b93149ae88106ad23c58e238a1867b8475b
osso2k <ossoble817@gmal.com>
2025-07-16 22:41:57 +0300

    Minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 
 const app = express()
+const PORT = 8000
 
 app.get('/', (req, res) => {
     res.send("Hello Mee")
 })
-app.listen(8000, () => console.log(`App is Running on Port 8000`))
+app.listen(PORT, () => console.log(`App is Running on http://localhost:${PORT}`))
