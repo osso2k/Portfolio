158f9fcb05fead614dc7501291252ff995f5e51b
osso2k <ossoble817@gmal.com>
2025-07-20 05:03:03 +0300

    redo

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-function genrand() {
-    return Math.floor(Math.random() * 100) + 1
-}
-console.log(genrand())
-
-export default genrand
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-const PORT = 8000
-
-app.get('/', (req, res) => {
-    res.send("Hello Mee")
-})
-app.listen(PORT, () => console.log(`App is Running on http://localhost:${PORT}`))
