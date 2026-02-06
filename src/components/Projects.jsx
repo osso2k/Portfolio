fc6a70a7150a422a0cc68190238c85e9dd22c71d
osso2k <ossoble817@gmail.com>
2026-02-06 06:12:00 +0300

    i'll fill the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,15 +20,35 @@ const Projects = () => {
             <p className='text-zinc-300 bg-gray-500 bg-opacity-25 px-2 py-1 text-sm rounded'>PostgreSQL</p>
           </div>
         </div>
-        <div className='bg-[hsl(0,0%,10%)] px-2 py-4 rounded-lg'>
-          <h1>JPrep  <span onClick={()=>{window.open("https://github.com/osso2k/JPrep", "_blank")}}>github</span></h1>
-          <img src={jprep} alt="" className=' rounded-lg' />
-          <p></p>
+        <div className='bg-[hsl(0,0%,6%)] px-2 py-4 rounded-lg '>
+          <div className='flex justify-between mb-5 border-b border-zinc-800 mx-5'>
+          <h1 className='italic text-2xl pr-2 font-semibold '>Resume Reviewer </h1> <span className='cursor-pointer hover:opacity-55 text-md  px-4 py-1 mb-2 bg-zinc-800 border border-black bg-opacity-35 text-zinc-600 rounded' onClick={()=>{window.open("https://github.com/osso2k/Schedly", "_blank")}}> source</span>
+          </div>
+          <img src={airesume} alt="" className=' rounded-lg w-fit object-contain border border-black ' />
+          <h2 className='text-md text-gray-300 mt-2'>Time managesment tool</h2>
+          <p className='font-medium text-sm my-2 text-gray-400'>I've always struggled with managing my time, so I built a simple app to help people plan their days and stay focused on what they want to get done. The app lets users write down tasks for each day and see them in a weekly view, like a digital planner. I built the interface using React and used Express on the backend to save user data. People can create accounts and log in, and their schedules are stored in a PostgreSQL database so they don’t lose their plans when they come back later.</p>
+          <div className='flex gap-1'>
+            <p className='text-cyan-400 bg-cyan-600 bg-opacity-25 px-2 py-1 text-sm rounded'>React</p>
+            <p className='text-blue-400 bg-blue-600 bg-opacity-25 px-2 py-1 text-sm rounded'>TypeScript</p>
+            <p className='text-green-700 bg-green-800 bg-opacity-25 px-2 py-1 text-sm rounded'>Node</p>
+            <p className='text-blue-300 bg-blue-500 bg-opacity-25 px-2 py-1 text-sm rounded'>Express</p>
+            <p className='text-zinc-300 bg-gray-500 bg-opacity-25 px-2 py-1 text-sm rounded'>PostgreSQL</p>
+          </div>
         </div>
-        <div className='bg-[hsl(0,0%,10%)] px-2 py-4 rounded-lg'>
-          <h1>Resume Reviewer  <span onClick={()=>{window.open("https://github.com/osso2k/ResumeReviewer", "_blank")}}>github</span></h1>
-          <img src={airesume} alt="" className=' rounded-lg' />
-          <p></p>
+        <div className='bg-[hsl(0,0%,6%)] px-2 py-4 rounded-lg '>
+          <div className='flex justify-between mb-5 border-b border-zinc-800 mx-5'>
+          <h1 className='italic text-2xl pr-2 font-semibold '>JPrep </h1> <span className='cursor-pointer hover:opacity-55 text-md  px-4 py-1 mb-2 bg-zinc-800 border border-black bg-opacity-35 text-zinc-600 rounded' onClick={()=>{window.open("https://github.com/osso2k/Schedly", "_blank")}}> source</span>
+          </div>
+          <img src={jprep} alt="" className=' rounded-lg w-fit object-contain border border-black ' />
+          <h2 className='text-md text-gray-300 mt-2'>Time managesment tool</h2>
+          <p className='font-medium text-sm my-2 text-gray-400'>I've always struggled with managing my time, so I built a simple app to help people plan their days and stay focused on what they want to get done. The app lets users write down tasks for each day and see them in a weekly view, like a digital planner. I built the interface using React and used Express on the backend to save user data. People can create accounts and log in, and their schedules are stored in a PostgreSQL database so they don’t lose their plans when they come back later.</p>
+          <div className='flex gap-1'>
+            <p className='text-cyan-400 bg-cyan-600 bg-opacity-25 px-2 py-1 text-sm rounded'>React</p>
+            <p className='text-blue-400 bg-blue-600 bg-opacity-25 px-2 py-1 text-sm rounded'>TypeScript</p>
+            <p className='text-green-700 bg-green-800 bg-opacity-25 px-2 py-1 text-sm rounded'>Node</p>
+            <p className='text-blue-300 bg-blue-500 bg-opacity-25 px-2 py-1 text-sm rounded'>Express</p>
+            <p className='text-zinc-300 bg-gray-500 bg-opacity-25 px-2 py-1 text-sm rounded'>PostgreSQL</p>
+          </div>
         </div>
     </div>
   )
